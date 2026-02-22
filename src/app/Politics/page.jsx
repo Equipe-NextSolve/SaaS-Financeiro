17576a49f7cbce212310fa0ed6f5b870bcff4d60
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-02-21 23:34:21 -0300

    Ajustes no Css - Responsividade
    Hospedei o site no Vercell para conseguir ter uma pré-visualização da responsividade do Site, e percebi que tinha algumas coisas quebradas para telas pequenas de IPhone.

--- src/app/Politics/page.jsx
+++ src/app/Politics/page.jsx
@@ -1,15 +1,13 @@
 "use client"
 import React from 'react'
 import styles from './Politics.module.css'
-import Carousel from './Components/Carousel/Carousel'
-import ArticlePolitics from './Components/Article/ArticlePolitics'
+import Link from 'next/link'
 
 export default function Politics() {
     return (
         <main className={styles.Politics}>
             <section className='divisionContent'>
-                <Carousel />
-                <ArticlePolitics />
+                
             </section>
         </main>
     )
